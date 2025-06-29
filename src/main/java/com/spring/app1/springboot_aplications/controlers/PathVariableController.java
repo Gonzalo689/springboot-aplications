8df081c276f701b09627351d4a964884d57e2a18
Gonzalo Contreras <gonzacd9@gmail.com>
2025-06-29 18:57:02 +0200

    value

--- src/main/java/com/spring/app1/springboot_aplications/controlers/PathVariableController.java
+++ src/main/java/com/spring/app1/springboot_aplications/controlers/PathVariableController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.spring.app1.springboot_aplications.models.Empleados;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +21,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/variable")
 public class PathVariableController {   
+    @Value("${config.usuario}")
+    private String usuario ;
+    @Value("${config.codigo}")
+    private int codigo ;
+    @Value("${config.mensaje}")
+    private String mensaje;
+    @Value("${config.valores}")
+    private String[] valores; 
+     
     @GetMapping("/pagina1/{informacion}")
     public ParametroDTO pagina1(@PathVariable String informacion) {
         ParametroDTO parametro = new ParametroDTO();
@@ -30,5 +43,14 @@ public class PathVariableController {
         return empleado1;
     }
     
+    @GetMapping("/valores")
+    public Map<String, Object> valores() {
+        return Map.of(
+            "usuario", usuario,
+            "codigo", codigo,
+            "mensaje", mensaje,
+            "valores", valores
+        );
+    }
 
 }
